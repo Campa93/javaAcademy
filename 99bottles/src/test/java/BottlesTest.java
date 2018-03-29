c494119c5b76ceb900923884ad45232a2ed6a99d
Mattia Campanini <cmpmattia@gmail.com>
2018-03-29 15:38:14 +0200

    characterization test

--- 99bottles/src/test/java/BottlesTest.java
+++ 99bottles/src/test/java/BottlesTest.java
@@ -2,17 +2,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class BottlesTest {
 
     @Test
-    public void testSong() {
+    public void testSong() throws IOException {
         final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         Bottles.writeLyrics(
                 new PrintStream(
                         byteArrayOutputStream));
         final String actual = new String(byteArrayOutputStream.toByteArray());
-        Assert.assertNotNull(actual);
+        final String expected = new String(Files.readAllBytes(Paths.get("src/main/resources/golden_master.txt")));
+
+        Assert.assertEquals(expected, actual);
     }
 }
