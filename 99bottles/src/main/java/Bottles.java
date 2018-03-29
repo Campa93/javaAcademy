ef29b0cd08671f2e77fbdf59cc7903619aed69ce
Mattia Campanini <cmpmattia@gmail.com>
2018-03-29 14:55:33 +0200

    moved variable out out of loop

--- 99bottles/src/main/java/Bottles.java
+++ 99bottles/src/main/java/Bottles.java
@@ -1,29 +1,24 @@
 import java.io.PrintStream;
 
-class Bottles
-{
-    public static void main(String args[])
-    {
+class Bottles {
+    public static void main(String args[]) {
         writeLyrics();
     }
 
     public static void writeLyrics() {
         String s = "s";
-        for (int beers=99; beers>-1;beers--)
-        {
-            final PrintStream out = System.out;
+        final PrintStream out = System.out;
+        for (int beers = 99; beers > -1; beers--) {
             out.print(beers + " bottle" + s + " of beer on the wall,");
             out.println(beers + " bottle" + s + " of beer,");
-            if (beers==0)
-            {
+            if (beers == 0) {
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
                 return;
-            }
-            else
+            } else
                 out.print("Take one down, pass it around,");
-            s = (beers-1 == 1)?"":"s";
-            out.println(beers-1 + " bottle" + s + " of beer on the wall.\r\n");
+            s = (beers - 1 == 1) ? "" : "s";
+            out.println(beers - 1 + " bottle" + s + " of beer on the wall.\r\n");
         }
     }
 }
